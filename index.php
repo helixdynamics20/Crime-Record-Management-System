a1df636fc8318485b9545b611d585f35a14cafa8
Vishal Kumar <36730768+helixdynamics20@users.noreply.github.com>
2019-11-15 18:24:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>NIA</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/best-carousel-slide.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/SBWP-Search-Bar-With-Parameters.css">
+    <link rel="stylesheet" href="assets/css/social-div-1.css">
+    <link rel="stylesheet" href="assets/css/social-div.css">
+    <link rel="stylesheet" href="assets/css/Social-Icons.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/WOWSlider-about-us-1.css">
+    <link rel="stylesheet" href="assets/css/WOWSlider-about-us-2.css">
+    <link rel="stylesheet" href="assets/css/WOWSlider-about-us.css">
+</head>
+
+<body class="body">
+    <header>
+        <div id="header"><img id="logo" src="assets/img/Header/National_Investigation_Agency_India_logo.png"><span class="text-white" id="initials">NIA</span><span class="text-light" id="fullname">NATIONAL INTELLIGENCE AGENCY</span></div>
+    </header>
+    <div class="container">
+        <div id="back">
+            <div id="left">
+                <h2 id="news">News and Features</h2>
+                <section id="carousel">
+                    <div class="carousel slide" data-ride="carousel" id="carousel-1">
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item">
+                                <div class="jumbotron pulse animated hero-nature carousel-hero">
+                                    <h1 class="hero-title">Hero Nature</h1>
+                                    <p class="hero-subtitle">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                                    <p><a class="btn btn-primary hero-button plat" role="button" href="#">Learn more</a></p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="jumbotron pulse animated hero-photography carousel-hero">
+                                    <h1 class="hero-title">Hero Photography</h1>
+                                    <p class="hero-subtitle">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                                    <p><a class="btn btn-primary hero-button plat" role="button" href="#">Learn more</a></p>
+                                </div>
+                            </div>
+                            <div class="carousel-item active">
+                                <div class="jumbotron pulse animated hero-technology carousel-hero">
+                                    <h1 class="hero-title">Hero Technology</h1>
+                                    <p class="hero-subtitle">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                                    <p><a class="btn btn-primary hero-button plat" role="button" href="#">Learn more</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-slide="prev"><i class="fa fa-chevron-left"></i><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-slide="next"><i class="fa fa-chevron-right"></i><span class="sr-only">Next</span></a></div>
+                        <ol
+                            class="carousel-indicators">
+                            <li data-target="#carousel-1" data-slide-to="0"></li>
+                            <li data-target="#carousel-1" data-slide-to="1"></li>
+                            <li data-target="#carousel-1" data-slide-to="2" class="active"></li>
+                            </ol>
+                    </div>
+                </section>
+            </div>
+            <div id="right">
+                <div id="info"><a class="text-white" href="#" target="_blank">Report Information</a></div>
+                <div id="contact"></div>
+                <h5 id="official">CONNECT WITH NIA</h5>
+                <div id="social"><i class="fa fa-facebook" id="spce"></i><i class="fa fa-twitter"></i><i class="fa fa-rss"></i><i class="fa fa-youtube-play"></i></div>
+                <div class="jumbotron" id="dir">
+                    <h1>Official</h1>
+                    <p>Paragraph</p>
+                    <p><a class="btn btn-primary text-white" role="button" href="admin.html" target="_parent">Admin</a></p>
+                    <p><a class="btn btn-primary text-white" role="button" href="login.html" target="_parent">User</a></p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="card"></div>
+            <h1 id="mw">Most Wanted</h1>
+            <div class="card-group">
+                <div class="card">
+                    <div class="mwij"><img class="iimg" src="assets/img/mw/1.jpg"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">&nbsp;Moosa Bin Suleiman</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary btcen" type="button">Read more...</button></div>
+                </div>
+                <div class="card">
+                    <div class="mwij"><img class="iimg" src="assets/img/mw/3.jpg"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">Robert William<br></h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary btcen" type="button">Read more...<br></button></div>
+                </div>
+                <div class="card">
+                    <div class="mwij"><img class="iimg" src="assets/img/mw/2.jpg"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">Alexis Flores</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary btcen" type="button">Read more...<br></button></div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h1 id="mw">Missing Person</h1>
+            <div class="card-group">
+                <div class="card">
+                    <div class="mwij"><img class="iimg" src="assets/img/index/preview.jpg"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">Tammy&nbsp;</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary btcen" type="button">Read more...<br></button></div>
+                </div>
+                <div class="card">
+                    <div class="mwij"><img class="iimg" src="assets/img/index/preview%20(4).jpg"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">Steve</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary btcen" type="button">Read more...<br></button></div>
+                </div>
+                <div class="card">
+                    <div class="mwij"><img class="iimg" src="assets/img/index/preview%20(3).jpg"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">Smith</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary btcen" type="button">Read more...<br></button></div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h1 id="mw">Seeking Information</h1>
+            <div class="card-group">
+                <div class="card">
+                    <div class="mwij"><img class="iimg" src="assets/img/7.png"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">Justin Travis</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary btcen" type="button">Read more...<br></button></div>
+                </div>
+                <div class="card">
+                    <div class="mwij"><img class="iimg" src="assets/img/index/preview%20(1).jpg"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">Saint Thomas</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary btcen" type="button">Read more...<br></button></div>
+                </div>
+                <div class="card">
+                    <div class="mwij"><img class="iimg" src="assets/img/index/kid.png"></div>
+                    <div class="card-body">
+                        <h4 class="card-title">Alexandra Anaya</h4>
+                        <p class="card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p><button class="btn btn-primary btcen" type="button">Read more...<br></button></div>
+                </div>
+            </div>
+        </div>
+        <div class="services">
+            <h1 id="ser">Services</h1>
+            <div class="row">
+                <div class="col">
+                    <div id="ncis">
+                        <div class="jumbotron">
+                            <h1>CJIS</h1>
+                            <p>Criminal Justice Information</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div id="cirg">
+                        <div class="jumbotron">
+                            <h1>CIRG</h1>
+                            <p>Critical Incident Response Group</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div id="ls">
+                        <div class="jumbotron">
+                            <h1>LS</h1>
+                            <p>Laboratory Services</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section>
+            <h1 id="investigate">What We Investigate</h1>
+            <div id="inleft">
+                <h1 id="terrh">Terrorism</h1>
+            </div>
+            <div id="inright">
+                <div id="upper">
+                    <h3 id="pchead">Public Corruption</h3>
+                </div>
+                <div id="lower">
+                    <h3 id="cch">Cyber Crime</h3>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="card"></div>
+        </section>
+    </div>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">Web design</a></li>
+                            <li><a href="#">Development</a></li>
+                            <li><a href="#">Hosting</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Company Name</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">NIA @ 2019</p>
+            </div>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/SBWP-Search-Bar-With-Parameters.js"></script>
+    <script src="assets/js/WOWSlider-about-us.js"></script>
+</body>
+
+</html>
